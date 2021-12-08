ab0326fe740b831674c0803db96af8bc32213ffa
Jairo Buendia <jairo@bicisbuendia.com>
2021-12-08 16:36:01 +0100

    Añadiendo páginas

--- src/app/services/prizes.service.ts
+++ src/app/services/prizes.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Prize} from '../model/prize';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrizesService {
+  prizes: Prize[] = [];
 
-  constructor() { }
+  constructor() { 
+    this.prizes = [
+      {"id":1,"name":"Limpiar la cocina","points":100,"image":""},
+      ];
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
 }
